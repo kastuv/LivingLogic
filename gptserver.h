76172dd4a0db98b779925d975f45b29df0074010
Kaustuv Pokharel <kastuvpokharel@gmail.com>
2024-03-15 03:31:15 -0400

    header values set

--- gptserver.h
+++ gptserver.h
@@ -7,11 +7,28 @@
 #include <QHostAddress>
 #include <QTimer>
 
-class gptserver
+class gptserver: public QObject
 {
     Q_OBJECT
 public:
-    gptserver();
+    explicit gptserver(QObject *parent =nullptr);
+    //To start a server
+    Q_INVOKABLE void startServer();
+
+signals:
+    void messageReceived(QString message);
+    //to know if we received a message
+
+private slots:
+    void newConnection();
+    void readyRead();
+    void checkForNewConnection();
+    //loop to chekc new connection
+
+private:
+    QTcpServer *tcpServer;
+    QTcpSocket *clientSocket;
+    QTimer *connectionTimer;
 };
 
 #endif // GPTSERVER_H
